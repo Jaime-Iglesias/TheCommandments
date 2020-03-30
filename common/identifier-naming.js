944f939ef5722deb58507bd112bf722fb057cdd4
Jaime Iglesias <22358726+Jaime-Iglesias@users.noreply.github.com>
2020-03-31 00:27:09 +0100

    Update identifier-naming.js

--- common/identifier-naming.js
+++ common/identifier-naming.js
@@ -2,8 +2,8 @@ function match(text, regex) {
     return text.replace(regex, '').length === 0
   }
 
-moduler.exports = {
+module.exports = {
     hasUnderscore(text) {
         return match(text, /_/)
-      }
+    }
 }
